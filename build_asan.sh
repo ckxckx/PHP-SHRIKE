0ecc6c72663d9a9ad12a2888154675dded3c2df5
Sean Heelan <sean@vertex.re>
2019-05-09 15:16:36 +0100

    ASAN build script

---
+++ build_asan.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+install_dir="$(pwd)/install-asan"
+make clean
+
+./configure --prefix="$install_dir" \
+    --enable-shrike --enable-dve --enable-mbstring --enable-intl \
+    --enable-exif --with-gd --with-bz2 CXXFLAGS="-DU_USING_ICU_NAMESPACE=1 "\
+    CFLAGS="-DU_USING_ICU_NAMESPACE=1 -O0 -g -fsanitize=address"
+
+make -j 5
+
+make install
